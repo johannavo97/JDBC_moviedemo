024bcc4a565f0a88b62e61fa7d7228522f6693af
Johanna Vo <thanhtruc2009@gmail.com>
2022-06-10 23:56:02 -0700

    First Commit

---
+++ src/main/java/org/example/models/Movie.java
@@ -0,0 +1,59 @@
+package org.example.models;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String genre;
+    private int yearOfRelease;
+
+    public Movie(String title, String genre, int yearOfRelease) {
+        this.title = title;
+        this.genre = genre;
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public Movie() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", yearOfRelease=" + yearOfRelease +
+                '}';
+    }
+}
