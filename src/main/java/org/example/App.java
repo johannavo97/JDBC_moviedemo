fa68dc4ca32223092d2abc8c420c0a241da7f6a2
Johanna Vo <thanhtruc2009@gmail.com>
2022-06-11 01:25:01 -0700

    finished

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.dao.MovieDAO;
+import org.example.models.Movie;
 import org.example.services.MovieDAOImpl;
 
 import java.sql.*;
@@ -14,9 +15,16 @@ public class App
         try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
             MovieDAO m = new MovieDAOImpl(conn);
             m.createTable();
-            m.createMovie();
-
-
+            m.createMovie(new Movie("Memory", "Action'", 2022));
+            m.createMovie(new Movie("Minions", "Comedy'", 2015));
+            m.createMovie(new Movie("Men", "Horror", 2022));
+            m.findMovieById(2).ifPresent(System.out::println);
+            m.findAll().forEach(System.out::println);
+            m.updateMoviesTitle(2, "Help");
+            m.findAll().forEach(System.out::println);
+            m.deleteMovie(3);
+            System.out.println(m.findAll().size()); // one row left in the table
+            m.deleteTable();
         } catch (SQLException e) {
             e.printStackTrace();
         }
