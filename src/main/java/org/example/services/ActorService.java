1e351eb20ddab7a141c71cf198a1fd866d3fac24
Johanna Vo <thanhtruc2009@gmail.com>
2022-06-14 16:19:46 -0700

    edit services

--- src/main/java/org/example/services/ActorService.java
+++ src/main/java/org/example/services/ActorService.java
@@ -2,6 +2,10 @@ package org.example.services;
 
 import org.example.dao.ActorI;
 import org.example.models.Actor;
+import org.example.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,21 +14,43 @@ import java.util.UUID;
 public class ActorService implements ActorI {
     @Override
     public Actor save(Actor a) {
-        return null;
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = s.beginTransaction();
+            s.persist(a);
+            tx.commit();
+        } catch (HibernateException exception) {
+            if (tx!=null) tx.rollback();
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
+        return a;
     }
 
     @Override
     public Optional<Actor> findById(UUID id) {
-        return Optional.empty();
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Optional<Actor> a = Optional.ofNullable(s.find(Actor.class, id));
+        s.close();
+        return a;
     }
 
     @Override
     public List<Actor> findAllBornAfter(int year) {
-        return null;
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        List<Actor> a = s.createQuery("SELECT a FROM actors a WHERE a.yearOfBirth > :year",Actor.class).setParameter("year", year).getResultList();
+        s.close();
+        return a;
     }
 
     @Override
     public List<Actor> findAllWithLastNameEndsWith(String lastNameEndsWith) {
-        return null;
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        List<Actor> a = s.createQuery("SELECT a FROM actors a WHERE a.lastName LIKE :lastName",Actor.class).setParameter("lastName", "%" + lastNameEndsWith).getResultList();
+        s.close();
+        return a;
     }
 }
--- src/main/java/org/example/services/GenreService.java
+++ src/main/java/org/example/services/GenreService.java
@@ -2,6 +2,10 @@ package org.example.services;
 
 import org.example.dao.GenreI;
 import org.example.models.Genre;
+import org.example.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,26 +14,62 @@ import java.util.UUID;
 public class GenreService implements GenreI {
     @Override
     public Genre createGenre(Genre g) {
-        return null;
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = s.beginTransaction();
+            s.persist(g);
+            tx.commit();
+        } catch (HibernateException exception) {
+            if (tx!=null) tx.rollback();
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
+        return g;
     }
 
     @Override
     public boolean deleteGenre(Genre g) {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = s.beginTransaction();
+            s.remove(g);
+            tx.commit();
+            return true;
+        } catch (HibernateException exception) {
+            if (tx!=null) tx.rollback();
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
         return false;
     }
 
     @Override
     public List<Genre> findAllByName(String name) {
-        return null;
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        List<Genre> g = s.createQuery("SELECT g FROM genres WHERE name = :name",Genre.class).setParameter("name", name).getResultList();
+        s.close();
+        return g;
     }
 
     @Override
     public Optional<Genre> findById(UUID id) {
-        return Optional.empty();
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Optional<Genre> g = Optional.ofNullable(s.find(Genre.class, id));
+        s.close();
+        return g;
     }
 
     @Override
     public List<Genre> findAll() {
-        return null;
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        List<Genre> g = s.createQuery("from genres ", Genre.class).getResultList();
+        s.close();
+        return g;
     }
 }
